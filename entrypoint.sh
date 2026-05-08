980de6ae7d9c1efdb1d57c605896fae71ecd2468
Liqiang Zhang <liqiang.zhang.m@hotmail.com>
2026-05-08 18:57:53 +0800

    添加环境变量修改label

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/act_runner register --ephemeral --instance ${GITEA_INSTANCE_URL} --token ${GITEA_RUNNER_REGISTRATION_TOKEN} --labels ubuntu-latest:host --no-interactive
+/usr/local/bin/act_runner register --ephemeral --instance ${GITEA_INSTANCE_URL} --token ${GITEA_RUNNER_REGISTRATION_TOKEN} --labels ${RUNNER_LABELS:-ubuntu-latest:host} --no-interactive
 exec /usr/local/bin/act_runner daemon --once
